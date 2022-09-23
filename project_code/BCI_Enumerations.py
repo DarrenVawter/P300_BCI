0fdcdd4423060ba28f52c1209bed79fe79a35865
DarrenVawter <47130509+DarrenVawter@users.noreply.github.com>
2022-09-23 16:36:20 -0700

    Added initial BCI enumerations

---
+++ project_code/BCI_Enumerations.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+This module is a header file for the program's enumerated data types.
+
+@author: Darren Vawter
+"""
+
+from enum import IntEnum; # Allow for integer enumeration types
+
+###########################
+#   Enumerate BCI Modes   #
+###########################
+class BCI_Mode(IntEnum):
+
+    # Default overlay which mirrors the full screen onto the P300 interface
+    DEFAULT_OVERLAY = 1;
+    # Magnified overlay which mirrors some portion of the screen onto the P300 interface
+    MAGNIFIED_OVERLAY = 2;
+    # Keyboard interface which displays a P300 keyboard to the user
+    KEYBOARD_INTERFACE = 3;
+    
+    """
+    Example specialty overlays forincreased data throughput:
+        SPECIAL_OVERLAY = N;
+        DESKTOP_OVERLAY = 10;
+        FILE_EXPLORER_OVERLAY = 11;
+        CHESS_OVERLAY = 12;
+        YOUTUBE_OVERLAY = 13;
+    """
+    
+#########################################
+#   Enumerate BCI Interaction Options   #
+#########################################
+class BCI_Interaction(IntEnum):
+    
+    ##########################
+    #   Mouse Interactions   #
+    ##########################
+    # Left-click the mouse at the center/selection of the current overlay
+    CLICK = 1;
+    # Doubl left-click the mouse at the center/selection of the current overlay
+    DOUBLE_CLICK = 2;
+    # Right-click the mouse at the center/selection of the current overlay
+    RIGHT_CLICK = 3;
+    
+    #############################
+    #   Keyboard Interactions   #
+    #############################
+    # Press the page up button
+    PAGE_UP = 10;
+    # Press the page down button
+    PAGE_DOWN = 11;
+    # Press the tab button
+    TAB = 12;
+    # Close keyboard
+    CLOSE_KEYBOARD = 13;
+    
+    #############################
+    #   Overlay Interactions   #
+    #############################
+    # Revert to the previous level of magnification
+    REVERT_MAGNIFICATION = 20;
+    
+    """
+    Example specialty interactions for increased throughput:
+        
+        ############################
+        #   Browser Interactions   #
+        ############################
+        # Browser back button
+        BROWSER_BACK = 30;
+        # Browser address bar
+        ADDRESS_BAR = 31;
+        # Browser refresh button
+        BROWSER_REFRESH = 32;  
+                
+    """
